974f3b45ce9cceee390c80aeb6e0063d5b380f76
ChickenHacker12 <treyeowen@gmail.com>
2019-10-12 13:45:59 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -48,7 +48,7 @@ request.onreadystatechange = function() {
 				// Create dead link
 				sponsorLink.setAttribute("href", "#");
 			}
-			
+
 			sponsorLink.setAttribute("title", sponsor.name);
 			if (sponsor.logoUrl != "") {
 				// Add logo to url
@@ -97,26 +97,4 @@ menuBox.addEventListener("click", () => {
   shown = 0;
   menuBox.removeAttribute("aria-expanded");
   menuBox.classList.remove("shown");
-});
-=======
-
-var menuBtn = document.getElementById("menu-btn");
-var menu = document.getElementsByTagName("nav")[0];
-var shown;
-menuBtn.addEventListener("click", () => {
-  if (shown == 1) {
-    //Hide menu
-    shown = 0;
-    menu.removeAttribute("aria-expanded");
-    menu.classList.remove("shown");
-  } else {
-    shown = 1;
-    menu.setAttribute("aria-expanded", "true");
-    menu.classList.add("shown");
-  }
-});
-menu.addEventListener("click", () => {
-  shown = 0;
-  menu.removeAttribute("aria-expanded");
-  menu.classList.remove("shown");
 });
