30887d6e1dba93890408efbdcad6ac008749748c
ChickenHacker12 <treyeowen@gmail.com>
2019-10-12 14:48:16 -0500

    Update load_settings.js

--- docs/settings/load_settings.js
+++ docs/settings/load_settings.js
@@ -10,12 +10,21 @@ request.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     // Load settings
     var settings = JSON.parse(this.responseText);
-    // general
+    // General
     document.title += " -" + settings.general.name;
     document.documentElement.lang = settings.general.language;
     var charset = document.createElement("meta");
     charset.setAttribute("charset", settings.general.charset);
     document.head.appendChild(charset);
+    // SEO
+    // Skip noindex, nofollow
+    var keywords = document.createElement("meta");
+    keywords.name = "keywords";
+    keywords.content = settings.seo.keywords;
+    document.head.appendChild(keywords);
+    var description = document.createElement("meta");
+    description.name = "description";
+    description.content = settings.seo.description;
 
   }
 }
