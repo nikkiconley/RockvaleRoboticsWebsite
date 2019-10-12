0dc9e9e014bd63746f6607b3ff7e973c261a111c
ChickenHacker12 <treyeowen@gmail.com>
2019-10-12 13:45:13 -0500

    Working on importing settings on page load

---
+++ docs/settings/load_settings.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2019 Trey Owen, and contributors
+// This document loads the settings from settings.json
+
+// Use document.body reference, etc.
+
+
+var request = new XMLHttpRequest(),
+url = "settings.json";
+request.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    // Load settings
+    var settings = JSON.parse(this.responseText);
+    // general
+    document.title += " -" + settings.general.name;
+    document.documentElement.setAttribute("lang", settings.general.language);
+    var charset = document.createElement("meta");
+    charset.setAttribute("charset", settings.general.charset);
+    document.head.appendChild(charset);
+  }
+}
+request.open("GET", url, true);
+request.send();
